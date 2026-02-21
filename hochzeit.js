bab2a1932f4956489d890cb861b0768e69a37c59
ilkay Pehlivanoglu <pehlivanogluilkay@gmail.com>
2026-02-21 10:07:19 +0100

    Files downloaded

---
+++ hochzeit.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector(".menuIcon-container");
+let navbar = document.querySelector(".nav");
+
+const showNavbar = () => {
+  
+    navbar.className = ".nav";
+    navbar.style.listStyle = "none";
+    
+  }
+menu.addEventListener("click", showNavbar);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000);
+}
